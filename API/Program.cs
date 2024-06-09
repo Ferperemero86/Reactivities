e399b4d35114bbd520f8ed2e59fe03dd7643045b
Ferperemero86 <fernandoperezmerono@gmail.com>
2024-06-09 09:50:38 +0100

    Configure db migrations

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,6 @@
+using Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(opt => {
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
@@ -17,5 +23,17 @@ if (app.Environment.IsDevelopment())
 }
 
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try {
+    var context = services.GetRequiredService<DataContext>();
+    context.Database.Migrate();
+
+} catch (Exception ex) {
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error ocured during migration");
+}
 
 app.Run();
